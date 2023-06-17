ce82ef30b684505a90c3b8b28b4880fdb19b93ab
arulgoyal <arulgoyal6@gmail.com>
2023-06-17 23:34:58 +0530

    whatGPT3_page

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ const App = () => {
   return (
     <div className='App'>
       <div className='gradient__bg'>
-        <Navbar />
-        <Header />
+      <Navbar />
+      <Header />
       </div>
       <Brand />
       <WhatGPT3 />
